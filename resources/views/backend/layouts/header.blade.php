928e0a6c01b89431e78631eb88b32ce53910b941
unknown <tjfahim1997@gmail.com>
2024-01-09 15:53:07 +0600

    favicon Updated

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -5,7 +5,8 @@
  <head>
      <meta charset="utf-8" />
      <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('backend/assets/img/apple-icon.png')}}">
-     <link rel="icon" type="image/png" href="{{ asset('backend/assets/img/favicon.ico')}}">
+     <link rel="icon" type="image/png" href="{{ asset('image/setting/' . $settings->favicon) }}">
+
      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
      <title>Admin</title>
      <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
@@ -28,12 +29,8 @@
                      
                      <div class="collapse navbar-collapse justify-content-end" id="navigation">
                          <ul class="nav navbar-nav mr-auto">
-                             
-                           
                          </ul>
                          <ul class="navbar-nav ml-auto">
-                             
-                            
                              <li class="nav-item">
                                  <a class="nav-link" href="{{ route('logout')}}">
                                      <span class="no-icon">Log out</span>
